92c07a40fe1cb9b8cd1d683b13b2a90a609302ae
shehan <shehan.peruma@gmail.com>
2017-11-29 23:19:23 -0500

    updated

--- example/src/test/java/DepartmentTest.java
+++ example/src/test/java/DepartmentTest.java
@@ -5,50 +5,56 @@ import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
+/**
+ * Refer: https://github.com/junit-team/junit4/wiki/exception-testing
+ */
 public class DepartmentTest {
 
-    Department d1, d2;
+    Department d1;
 
     @Before
     public void setUp() {
         d1 = new Department("HR");
-        d2 = new Department("IT");
     }
 
     @Test
     public void getName() {
-        assertEquals("Gets the department name", "HR", d1.getName());
+        assertEquals("Test to get the Department name", "HR", d1.getName());
     }
 
     @Test
     public void addEmployee() {
         d1.AddEmployee(1);
-        assertTrue(d1.hasEmployee(1));
+        assertTrue("Test to add an Employee",d1.hasEmployee(1));
     }
 
     @Test
     public void removeEmployee() {
         d1.AddEmployee(2);
         d1.RemoveEmployee(2);
-        assertFalse(d1.hasEmployee(2));
+        assertFalse("Test to remove and Employee",d1.hasEmployee(2));
     }
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
-    public void shouldTestExceptionMessage() {
+    /**
+     * This test method expects an exception. It checks the exception type and message
+     * This is part of JUnit 4
+     */
+    public void AddEmployeeException() throws MyCustomException {
         thrown.expect(MyCustomException.class);
         thrown.expectMessage("AddEmployee - Failure");
 
         d1.AddEmployee(3);
         d1.AddEmployee(3);
-        d1.AddEmployee(4);
     }
 
     @Test(expected = MyCustomException.class)
     /**
-     * This test method expects an exception (i.e. the test method will pass if the NullPointerException is thrown).
+     * This test method expects an exception (i.e. the test method will pass if the MyCustomException is thrown).
+     * This is part of JUnit 4
      */
     public void removeEmployeeException() {
         d1.RemoveEmployee(5);
@@ -63,7 +69,7 @@ public class DepartmentTest {
         try {
             d1.RemoveEmployee(5);
         } catch (Exception error) {
-            assertEquals(error.getClass(), MyCustomException.class);
+            assertEquals("Test to verify that removing an employee throws MyCustomException",error.getClass(), MyCustomException.class);
         }
     }
 
